5d96454b6628448622b94759da9e8ae902ddd9d2
carlmstott <124844573+carlmstott@users.noreply.github.com>
2023-09-20 20:15:45 -0500

    Update PythonSideLineFollowing.py
    lines_hit

--- Lab3AdvMech/FullLineFollowingAllCode/PythonSideLineFollowing.py
+++ Lab3AdvMech/FullLineFollowingAllCode/PythonSideLineFollowing.py
@@ -9,8 +9,7 @@ rightMotor=int(100)
 
 if __name__ == '__main__':
     ser=serial.Serial('/dev/ttyACM0',115200)
-    #every time the serial port is opened, the arduino program will restart
-    ser.reset_input_buffer()
+    ser.reset_input_buffer() #clears anything the arduino has been sending while the Rpi isnt prepared to recieve.
 
     while True:
         sendString('/dev/ttyACM0',115200,'<'+str(leftMotor)+','+str(rightMotor)+'>',0.0001)
@@ -18,14 +17,13 @@ if __name__ == '__main__':
         if ser.in_waiting > 0:  
                  
                 line = ser.readline().decode('utf-8')
-                # print(line)
                 line=line.split(',')
                 #this splits the incoming string up by commas
                 try:
                     
                     x=int(line[0])
                     y=int(line[1])
-                    z=line[2] #we dont convert this to a float becasue we went to be able to recieve the message that we are at a cross, which wont be an int
+                    z=line[2] #we dont convert this to a float becasue we went to be able to recieve the message that we are at a cross, which wont be an int. 
                     print([x,y,z])
                 except:
                     print("packet dropped") #this is designed to catch when python shoves bits on 
@@ -37,8 +35,8 @@ if __name__ == '__main__':
                 #below is a basic control law you can send to your motors, with an exeption if z is the string 'cross' and not a lineposition
             
                 if not z == 'cross': #im assuming that in your arduino code you will be setting z to the string 'cross' if you sense a cross, dont feel obligated to do it this way.  
-                    leftMotor=100+.02*float(z)
-                    rightMotor=250-.02*float(z)
+                    leftMotor=100+.02*int(z) #now that we are SURE that z isbt the string cross, we cast z to an int and recalculate leftMotor and rightMotor, 
+                    rightMotor=250-.02*int(z)
                 #else:
-                    #do something here like setting a value you call 'lineshit' to one higher, then writing code to make sure that some time  (1 second should do it) 
-                    # passes between being able to incriment lineshit so that it wont be incrimented a bunch of times when you hit your first cross
+                    #do something here like setting a value you call 'lines_hit' to one higher, and writing code to make sure that some time (1 second should do it) 
+                    # passes between being able to incriment lines_hit so that it wont be incrimented a bunch of times when you hit your first cross
